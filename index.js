4dc6c3fabeaced0c83bc3bc5033e10fdae8fba3a
GUL-Bao <BaoPham92@yahoo.com>
2019-10-28 16:21:51 -0400

    Initial setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// implement your API here
+// * PKGS
+const express = require('express')
+const server = express()
+const port = 5000;
+
+// * DATABASE
+const db = require('./data/db')
+
+// * MIDDLEWARE
+server.use(express.json())
+
+// * ROUTES
+
+// ? LISTENER
+server.listen(
+    port,
+    () => console.log(`Listening to port: ${port}`)
+)
