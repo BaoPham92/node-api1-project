46f199f8159a223d863991dc753406379bcb9cfb
GUL-Bao <BaoPham92@yahoo.com>
2019-10-28 17:54:49 -0400

    POST feature added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.get('/', (req, res) => {
 server.get('/api/users', (req, res) => {
     db.find()
         .then(users => res.status(200).json(users))
-        .catch(err => res.json({ error: "Error requesting \'/api/users\' " }))
+        .catch(err => res.json({ errorMessage: "Error requesting \'/api/users\' " }))
 })
 
 // ? GET '/api/users/:id'
@@ -30,8 +30,23 @@ server.get('/api/users/:id', (req, res) => {
             res.status(200).json(user)
         )
         .catch(err => res.status(404)
-            .json({ error: "The user with the specified ID does not exist." })
+            .json({ errorMessage: "The user with the specified ID does not exist." })
         )
+})
+
+// ? POST 'api/users/'
+server.post('/api/users', (req, res) => {
+    const user = req.body
+
+    if (!!user.name === false || !!user.bio === false) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.insert(user)
+            .then(data => res.status(201).json(data))
+            .catch(err => res.status(500)
+                .json({ errorMessage: "There was an error while saving the user to the database" })
+            )
+    }
 })
 
 // ? LISTENER
